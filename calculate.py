6316553f434597669c12589d1f48d6c9b6d594d5
Mohd Shafizan Bin Abdullah <yodeh9999@gmail.com>
2023-02-07 16:23:37 +0800

    Perubahan kedua

--- calculate.py
+++ calculate.py
@@ -4,3 +4,5 @@ tax = float(input("tax percent\n"))
 amount = price * tax / 100
 totalamount = price + amount
 print("HI " + name + ", the total amount to pay is RM" + str(totalamount) + ", the sales tax is " + str(tax) + " percent")
+
+name = "Sang"
