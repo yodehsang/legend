c90b79fa83e2cba279865e8ebd0c1aa5dd0ba7b9
Mohd Shafizan Bin Abdullah <yodeh9999@gmail.com>
2023-02-13 14:21:06 +0800

    Perubahan ketiga

---
+++ gred.py
@@ -0,0 +1,14 @@
+name = input("Please enter your name: ")
+mark = int(input("Please enter your mark: "))
+
+if mark >= 80:
+    gred = ("you are brilliant")
+    print("Hi " + ", your mark " + str(mark) + ", " + str(gred))
+    
+    
+
+elif mark >= 60:
+    gred1 = ("you did okay. Better luck next time")
+    print("Hi " + ", your mark " + str(mark) + ", " + str(gred1))
+
+    
---
+++ weather.py
@@ -0,0 +1,14 @@
+tempereture = float(input("Enter your tempereture: "))
+if tempereture > 80:
+    print("It's too Hot!!")
+    print("Stay Inside")
+
+elif tempereture < 55:
+    print("Still quite Hot!!")
+
+elif tempereture < 20:
+    print("It's too cold")
+
+else:
+    print("Have a Nice Day")
+
